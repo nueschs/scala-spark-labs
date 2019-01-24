5628195ddbb1906675ec28eda8e6e297163bf933
snuesch <stefan.nueesch@mgb.ch>
2019-01-24 10:14:35 +0100

    Lab 5 removed solutions from master

--- src/main/scala/com/scigility/lab/lab05/Lists.scala
+++ src/main/scala/com/scigility/lab/lab05/Lists.scala
@@ -11,27 +11,26 @@ object Lists {
 
   // transform all first names in the list above to lower case
   // s.t. lower(firstNames) == List("cersei", "ford", "ellen", "darth", "arthur", "john")
-  def lower(names: List[String]): List[String] = names.map(_.toLowerCase())
+  def lower(names: List[String]): List[String] = ???
 
   // create a list which contains the length of each of the names in firstNames in the correct order
   // s.t. lengths(firstNames) == List(6, 4, 5, 5, 6, 4)
-  def lengths(names: List[String]): List[Int] = names.map(_.length)
+  def lengths(names: List[String]): List[Int] = ???
 
   // make a list of all characters in order of their appearance in firstNames
   // s.t. characters(firstNames) == List('C','e','r','s','e','i','F',...)
-  def characters(names: List[String]): List[Char] = names.flatMap(_.toList)
+  def characters(names: List[String]): List[Char] = ???
 
   // remove duplicates from the above list of characters
   // there is a specific function for this available on list, but try to build it using foldLeft or something else first
-  def uniqueCharacters(names: List[String]): List[Char] = characters(names)
-    .foldLeft(List.empty[Char])((acc, c) => if (!acc.contains(c)) c :: acc else acc)
+  def uniqueCharacters(names: List[String]): List[Char] = ???
 
   // write a function which converts a list of persons to a list of their first names
-  def firstNames(persons: List[Person]): List[String] = persons.map(_.firstName)
+  def firstNames(persons: List[Person]): List[String] = ???
 
   // write a function which takes a list of persons and returns the list of persons in that list which are older than
   // the minimal age
-  def oldPeople(persons: List[Person], minAge: Int): List[Person] = persons.filter(_.age >= minAge)
+  def oldPeople(persons: List[Person], minAge: Int): List[Person] = ???
 
   /*
    * From lists of first names, last names and ages, define a function which constructs a list of persons
@@ -46,11 +45,7 @@ object Lists {
 	 *     Person("John", "Snow", 22)
    *   )
    */
-  def constructPeopleList(firstNames: List[String], lastNames: List[String], ages: List[Int]): List[Person] ={
-    (firstNames zip lastNames zip ages) map {
-      case ((firstName, lastName), age) => Person(firstName, lastName, age)
-    }
-  }
+  def constructPeopleList(firstNames: List[String], lastNames: List[String], ages: List[Int]): List[Person] = ???
 }
 
 
