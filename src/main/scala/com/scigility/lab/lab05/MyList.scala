2b8fd4ac2e00c5a61b6a445e0610b56e6a627c09
snuesch <stefan.nueesch@mgb.ch>
2019-01-28 09:26:35 +0100

    added all list solutions to make tests run

--- src/main/scala/com/scigility/lab/lab05/MyList.scala
+++ src/main/scala/com/scigility/lab/lab05/MyList.scala
@@ -16,17 +16,33 @@ object MyList {
     *
     * @return the list of all elements in lst which satisfy the predicate
     */
-  def filter[A](lst: MyList[A])(predicate: A => Boolean): MyList[A] = ???
+  def filter[A](lst: MyList[A])(predicate: A => Boolean): MyList[A] = lst match {
+    case MyNil() => MyNil[A]
+    case MyCons(head, tail) => if (predicate(head)) MyCons(head, filter(tail)(predicate)) else filter(tail)(predicate)
+  }
 
   /**
     * Simultaneously traverses lstA and lstB zipping the lists
     *
     * @return a list of pairs (A, B) of the elements at the same index in each list
     */
-  def zip[A, B](lstA: MyList[A], lstB: MyList[B]): MyList[(A, B)] = ???
+  def zip[A, B](lstA: MyList[A], lstB: MyList[B]): MyList[(A, B)] = (lstA, lstB) match {
+    case (MyNil(), MyNil()) => MyNil[(A,B)]
+
+    // We decided to handle lists of different lengths gracefully, i.e. zip all elements of the shorter list with
+    // length n with the first n elements of the longer list, and discard the left overs (sam behaviour as in the
+    // scala standard library)
+    case (MyNil(), MyCons(headB, tailB)) => MyNil[(A, B)]
+    case (MyCons(headA, tailA), MyNil()) => MyNil[(A, B)]
+
+    case (MyCons(headA, tailA), MyCons(headB, tailB)) => MyCons((headA, headB), zip(tailA, tailB))
+  }
 
   /**
     * Traverses a list from left to right, accumulating the result of f and starting with "zero" value b
     */
-  def foldLeft[A, B](lst: MyList[A], b: B)(f: (B, A) => B): B = ???
+  def foldLeft[A, B](lst: MyList[A], b: B)(f: (B, A) => B): B = lst match {
+    case MyNil() => b
+    case MyCons(head, tail) => foldLeft(tail, f(b, head))(f)
+  }
 }
