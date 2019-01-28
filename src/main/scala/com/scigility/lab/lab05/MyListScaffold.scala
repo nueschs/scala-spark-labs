ba47014578dcd8193ff61b1c845f2022cf81773e
snuesch <stefan.nueesch@mgb.ch>
2019-01-28 09:26:35 +0100

    added all list solutions to make tests run

--- src/main/scala/com/scigility/lab/lab05/MyListScaffold.scala
+++ src/main/scala/com/scigility/lab/lab05/MyListScaffold.scala
@@ -11,27 +11,26 @@ object MyListScaffold {
     case MyCons(head, tail) => MyCons(f(head), myMap(tail)(f))
   }
 
-  // hint: look at the recursion used in myMap above.
   def filter[A](lst: MyList[A])(predicate: A => Boolean): MyList[A] = lst match {
-    case MyNil() => ???
-    case MyCons(head, tail) => if (predicate(head)) ??? else ???
+    case MyNil() => MyNil[A]
+    case MyCons(head, tail) => if (predicate(head)) MyCons(head, filter(tail)(predicate)) else filter(tail)(predicate)
   }
 
   def zip[A, B](lstA: MyList[A], lstB: MyList[B]): MyList[(A, B)] = (lstA, lstB) match {
-    case (MyNil(), MyNil()) => ???
+    case (MyNil(), MyNil()) => MyNil[(A,B)]
 
-    // The next two cases handle lists of different lengths. Think about how to handle those
-    case (MyNil(), MyCons(headB, tailB)) => ???
-    case (MyCons(headA, tailA), MyNil()) => ???
+    // We decided to handle lists of different lengths gracefully, i.e. zip all elements of the shorter list with
+    // length n with the first n elements of the longer list, and discard the left overs (sam behaviour as in the
+    // scala standard library)
+    case (MyNil(), MyCons(headB, tailB)) => MyNil[(A, B)]
+    case (MyCons(headA, tailA), MyNil()) => MyNil[(A, B)]
 
-    case (MyCons(headA, tailA), MyCons(headB, tailB)) => ???
+    case (MyCons(headA, tailA), MyCons(headB, tailB)) => MyCons((headA, headB), zip(tailA, tailB))
   }
 
-
   def foldLeft[A, B](lst: MyList[A], b: B)(f: (B, A) => B): B = lst match {
-    case MyNil() => ???
-    case MyCons(head, tail) => ??? // foldLeft(???)
-
+    case MyNil() => b
+    case MyCons(head, tail) => foldLeft(tail, f(b, head))(f)
   }
 
 }
