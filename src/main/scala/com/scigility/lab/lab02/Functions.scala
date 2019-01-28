83b55014a9827889d3ab9bb4d31fb927c3cc964e
snuesch <stefan.nueesch@mgb.ch>
2019-01-28 09:23:24 +0100

    removed solutions after merge

--- src/main/scala/com/scigility/lab/lab02/Functions.scala
+++ src/main/scala/com/scigility/lab/lab02/Functions.scala
@@ -1,8 +1,5 @@
 package com.scigility.lab.lab02
 
 object Functions {
-  def factorial(i:Int):Int = i match {
-    case 0 => 1
-    case k => k * factorial(k-1)
-  }
+  def factorial(i:Int):Int = ???
 }
